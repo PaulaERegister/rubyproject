4c5abf29df6d5978ce4b57314820cce8cac994e4
PaulaERegister <per4521@g.rit.edu>
2019-10-04 17:46:06 -0400

    Override devise registration with custom bootstrap form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   root to: 'pages#index'
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+  # devise_scope :user do
+  #   get 'signup', to 'devise/registrations#new'
+  # end
 end
