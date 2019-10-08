a99d1c749df91ca509ca32c58da7346ddeeabf73
PaulaERegister <per4521@g.rit.edu>
2019-10-08 11:45:32 -0400

    Created templates for hobby, study, team.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,14 @@
 module PostsHelper
+
   def create_new_post_partial_path
-    if user_signed_in?
-      'posts/branch/create_new_post/signed_in'
-    else
-      'posts/branch/create_new_post/not_signed_in'
-    end
+    user_signed_in? ? 'posts/branch/create_new_post/signed_in' : 'posts/branch/create_new_post/not_signed_in'
+  end
+
+  def all_categories_button_partial_path
+    params[:category].blank? ? 'posts/branch/categories/all_selected' : 'posts/branch/categories/all_not_selected'
+  end
+
+  def no_posts_partial_path
+    @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
   end
 end
