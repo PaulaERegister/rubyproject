499087f5d777f463af7dbba2f96e891cf960c94c
PaulaERegister <per4521@g.rit.edu>
2019-10-08 15:47:48 -0400

    Added posts from branches into the home page.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -23,4 +23,8 @@ module PostsHelper
   def update_pagination_partial_path
     @posts.next_page ? 'posts/posts_pagination_page/update_pagination' : 'posts/posts_pagination_page/remove_pagination'
   end
+
+  def no_posts_partial_path(posts)
+    posts.empty? ? 'posts/shared/no_posts' : 'shared/empty_partial'
+  end
 end
