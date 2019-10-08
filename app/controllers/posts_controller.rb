574e616a666a0182c029445d714345af5230bbb7
PaulaERegister <per4521@g.rit.edu>
2019-10-08 10:13:16 -0400

    Added routes.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,16 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def hobby
+    post_for_branch(params[:action])
+  end
+
+  def study
+    post_for_branch(params[:action])
+  end
+
+  def team
+    post_for_branch(params[:action])
+  end
 end
