88d24dee8617a7db4bc93ba1d081aa937acf9af3
PaulaERegister <per4521@g.rit.edu>
2019-10-08 21:22:15 -0400

    Can send messages and view current conversations.

--- app/models/private/conversation.rb
+++ app/models/private/conversation.rb
@@ -9,5 +9,7 @@ class Private::Conversation < ApplicationRecord
   scope :between_users, -> (user1_id, user2_id) do
     where(sender_id: user1_id, recipient_id: user2_id).or(where(sender_id: user2_id, recipient_id: user1_id))
   end
-
+  def opposite_user(user)
+    user == recipient ? sender : recipient
+  end
 end
